ba27275afa87541db0f4f48b24fd57ce5d9984cd
andre6033 <55622859+andre6033@users.noreply.github.com>
2019-09-21 14:14:12 +0300

    Add files via upload

---
+++ bootdent.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Стоматологическая клиника</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="animate.css">
+    <link rel="stylesheet" type="text/css" href="owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="owlcarousel/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="style.boot.css">
+</head>
+<body>
+<div id="requreHeader">
+<?php require "header1.php"?></div>
+<div class="main">
+     <div class="castom1 owl-carousel">
+             <div class="item"><img src="img/image%203.jpg"></div>
+     </div>
+</div>
+<div class="main_center container-fluid">
+    <div class="container">
+        <div class="servis">
+            <h2 id="serv">Услуги стоматологической клиники в Калининграде</h2>
+            <div class="list_servis">
+                <div class="list1 row">
+                    <div class="col mr-3">
+                        <h3>Лечение зубов </h3>
+                        <ul>
+                            <li>Лечение кариеса</li>
+                            <li>Лечение нервов зуба</li>
+                            <li>Лечение каналов зуба</li>
+                            <li>Лечение пульпита</li>
+                            <li>Лечение периодонтита</li>
+                        </ul>
+                    </div >
+                    <div class="col mr-3">
+                        <h3>Профессиональная гигиена</h3>
+                        <ul>
+                            <li> Полировка зубов</li>
+                            <li>Реминерализация</li>
+                            <li>Лечение гиперстезии</li>
+                            <li>Снятие пигментированного налета</li>
+                        </ul>
+                    </div>
+                    <div class="col mr-3">
+                        <h3>Имплантация </h3>
+                        <ul>
+                            <li>Имплантаты Hi-Tec</li>
+                            <li>Имплантаты Mis</li>
+                            <li> Базальная имплантация</li>
+                            <li>Быстрая имплантация</li>
+                            <li>Дентальные имплантаты</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="list2 row">
+                    <div class="col mr-3">
+                        <h3>Исправление прикуса</h3>
+                        <ul>
+                            <li> Брекеты и брекет-системы</li>
+                            <li>Исправление прикуса без брекетов</li>
+
+                        </ul>
+                    </div>
+                    <div class="col mr-3">
+                        <h3> Удаление зубов</h3>
+                        <ul>
+                            <li> Удаление зуба мудрости</li>
+                            <li> Удаление под общим наркозом</li>
+
+                        </ul>
+                    </div>
+                    <div class="col mr-3">
+                        <h3>Протезирование</h3>
+                        <ul>
+                            <li> Коронки</li>
+                            <li>Съемное протезирование</li>
+                            <li>Полное протезирование</li>
+
+                        </ul>
+                    </div>
+                    <div class="col mr-3">
+                        <h3>Собственная лаборатория</h3>
+                        <ul>
+                            <li> Анализы</li>
+                            <li>Анализы</li>
+                            <li>Анализы</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--end container-->
+</div>
+<div class='main_bottom'>
+    <div class="container">
+        <div class="history">Наша стоматологическая клиника основана в 1998 году и оснащена современным оборудованием для
+            оказания экстренной амбулаторной медицинской помощи, а также плановой эстетической реабилитации пациентов.
+            Команда высококвалифицированных специалистов поможет Вам обрести красивую улыбку и здоровые зубы. Наши
+            доктора
+            имеют международное образование и являются действующими научными сотрудниками. Мы не отправляем пациентов,
+            не
+            оказав им помощь в реабилитации и не отказываемся от сложных клинических случаев, а наоборот, охотно берёмся
+            за
+            них.<br><br>
+            Главная цель клиники стоматологии «Весна» — делать вашу улыбку совершенной. Наша клиника неоднократно
+            завоёвывала статус лучшей стоматологии в региональных и национальных конкурсах, что подтверждает качество
+            работы
+            специалистов центра. Персональный подход к вашей ситуации, внимательный и высококвалифицированный персонал
+            помогут решить вашу проблему и принесут исключительно положительные эмоции. Каждый врач нашего центра —
+            сертифицированный специалист с многолетним опытом работы и большим количеством постоянных клиентов. Мы
+            искренне
+            любим свою работу и выполняем её на высоком профессиональном уровне!
+        </div>
+    </div><!--end container-->
+</div>
+<div class="kart">
+    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af3c8f77def2eed67689994a1d574a0eb49bb53c5ff9d89b5ecc56e41ef6e69c5&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+</div>
+<div class="main_footer">
+    <div class="container">
+        <footer>
+            <div class="foot1">
+                <div><p><img src="img/tel.png"><a href="tel:+7 (4012) 39-11-19">+7 (4012) 39-11-19</a></div>
+                <div><p><img src="img/Vector1.png"><span>Калининград, ул. Виллима Фермора, 3</span></p></div>
+                <div><p><img src="img/Vector.png"><span>Пн-Пт 09:00 до 20:00<br> Сб 10:00 до 14:00</span> </p></div>
+                <div><p>© 2019</p><p>Центр стоматологии «Весна»</p></div></div>
+            <div class="foot2">
+                <h3>Стоматология</h3>
+                <ul>
+                    <li>О клинике </li>
+                    <li>Услуги</li>
+                    <li>Наши врачи</li>
+                    <li>График работы </li>
+                    <li>Цены</li>
+                    <li>Отзывы</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+            <div class="foot2">
+                <h3>Услуги</h3>
+                <ul>
+                    <li>Профессиональная гигиена</li>
+                    <li>Лечение зубов </li>
+                    <li>Удаление зубов</li>
+                    <li>Имплантация </li>
+                    <li>Протезирование</li>
+                    <li>Исправление прикуса</li>
+                    <li>Собственная лаборатория</li>
+                </ul>
+            </div>
+        </footer>
+
+        <div class="end">
+            <div>
+                <img src="img/vk.svg">
+                <img src="img/ok.svg">
+                <img src="img/face.svg">
+                <img src="img/insta.svg">
+            </div>
+            <p>Политика обработки персональных данных</p>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script type="text/javascript" src="owl.carousel.js"></script>
+<script type="text/javascript" src="owl.carousel.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script type="text/javascript" src="owlcarousel/src/js/owl.animate.js"></script>
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
