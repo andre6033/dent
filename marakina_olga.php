38364f789c4d13f0432981b7ccda5fac1fb70d6d
andre6033 <55622859+andre6033@users.noreply.github.com>
2019-09-21 14:37:32 +0300

    Add files via upload

---
+++ marakina_olga.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="animate.css">
+    <link rel="stylesheet" href="style.boot.css">
+    <title>Маракина Ольга Анатольевна</title>
+</head>
+<body>
+<?php $doctor = 'Маракина Ольга Анатольевна'?>
+<?php  require 'header1.php'?>
+<div class="main">
+    <div class="container">
+        <?php require 'modal1.php'?>
+        <div class=" inf_doc row">
+            <div class="col-4 md-3"><img src="img/marakina.jpg"></div>
+        <div class="col-8 md-3"> <h2><?php echo $doctor ?></h2>
+            <h6>Специализация</h6>
+            <p>Врач-стоматолог-ортодонт</p>
+            <h6>Образование</h6>
+            <p>Окончила Волгоградскую медицинскую академию по специальности Врач-стоматолог. В настоящее время заканчивает аспирантуру.</p>
+            <h6>Повышение квалификации</h6>
+            <p>2002 г. — МГМСУ — Курс «Обезболивание и неотложная помощь в стоматологии»;</p>
+            <p>2004 г. — МГМСУ — Курс «Стоматология ортопедическая»;</p>
+            <p>2005 г. — курс «Актуальные вопросы ортопедической стоматологии»;</p>
+            <p>2007 г. — лекционно-практический курс компании Kerr «Инновационные технологии в эстетической стоматологии»;</p>
+            <p>2007 г. — участник конференции " Умная Эстетика" с участием доктора Дэвида Винклера (Великобритания);</p>
+            <p>5 декабря 2013 г. принял участие в международной образовательной конференции по имплантологии и эстетической стоматологии «Актуальные концепции эстетическрй реабилитации на имплантах»</p>
+        </div>
+            <input type='button' id="doctor" value="Я хочу записаться на прием к данному специалисту">
+    </div>
+    </div>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
